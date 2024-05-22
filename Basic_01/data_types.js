4046130a04dfba0876ca2533165b01d5d01489be
Mirza Abdullah Baig <abaig7921@gmail.com>
2024-05-22 15:27:57 +0500

    data_types

---
+++ Basic_01/data_types.js
@@ -0,0 +1,28 @@
+"use strict"; // Treat all js code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3+3); console.log("Abdullah"); // Higly not recommended beacuse code redebability should be highly recommended
+
+console.log(3+3) // Recommended
+console.log("Abdullah") // Recommended
+
+let name = "Abdullah"
+let age = 20
+let isPresent = false
+let city;
+let temperature = null
+
+
+// number => 2 to power of 53
+// bigint
+// String => ""
+// Boolean => true/false
+// null => standalone value
+// Undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // null
