a8b6f07058ea32914a63f6fd75f4da6504028ac1
Mirza Abdullah Baig <abaig7921@gmail.com>
2024-05-25 15:56:39 +0500

    Practice

---
+++ Basic_01/practice.js
@@ -0,0 +1,10 @@
+let undtype_ = typeof undefined
+
+console.log(typeof undefined); // undefined
+console.log(typeof undtype_); // string
+
+
+let nulltype_ = typeof null
+
+console.log(typeof null); // object
+console.log(typeof nulltype_); // string
