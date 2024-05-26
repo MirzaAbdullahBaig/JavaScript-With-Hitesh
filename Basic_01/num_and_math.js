d815258352678429d0b1f6a106c02086d879205a
Mirza Abdullah Baig <abaig7921@gmail.com>
2024-05-26 11:29:57 +0500

    Num & Math's

--- Basic_01/num_and_math.js
+++ Basic_01/num_and_math.js
@@ -1,3 +1,5 @@
+// ************ Nums ***********
+
 const score = 400;
 console.log(score);
 
@@ -9,6 +11,45 @@ console.log(balance.toString().length);
 console.log(balance.toFixed(2));
 
 
-const otherNumber = 823.4545
+const otherNumber1 = 823.4545
+const otherNumber2 = 8233.4545
 
-console.log(otherNumber.toPrecision(3));
+console.log("🚀 ~ otherNumber1:", otherNumber1.toPrecision(3))
+console.log("🚀 ~ otherNumber2:", otherNumber2.toPrecision(3))
+
+
+const hundered = 1000000;
+console.log("🚀 ~ hundered:", hundered.toLocaleString())
+
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MIN_SAFE_INTEGER);
+console.log(Number.EPSILON);
+console.log(Number.isFinite());
+console.log(Number.isInteger());
+console.log(Number.NaN);
+
+
+
+
+// ************ Math's ***********
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.4));
+console.log(Math.ceil(4.1));
+console.log(Math.floor(4.9));
+console.log(Math.sqrt(2));
+console.log(Math.min(3, 4, 6, 2, 9, 0));
+
+console.log(Math.random());
+console.log(Math.random() * 10 + 1);
+console.log((Math.random() * 10) + 1);
+console.log(Math.ceil((Math.random() * 10)));
+
+
+const min = 30
+const max = 50
+
+console.log(Math.ceil(Math.random() * (max - min)) + min);
