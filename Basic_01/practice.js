991236302d24bd54923d4d5863593c265df04198
Mirza Abdullah Baig <abaig7921@gmail.com>
2024-05-26 11:30:07 +0500

    Practice

--- Basic_01/practice.js
+++ Basic_01/practice.js
@@ -1,10 +1,19 @@
-let undtype_ = typeof undefined
+const undtype_ = typeof undefined
 
 console.log(typeof undefined); // undefined
 console.log(typeof undtype_); // string
 
 
-let nulltype_ = typeof null
+const nulltype_ = typeof null
 
 console.log(typeof null); // object
 console.log(typeof nulltype_); // string
+
+
+
+console.log(typeof Number.score); // undefined
+console.log(typeof Number.name); // string
+console.log(typeof Number.isNaN); // function
+console.log(typeof Number.isNaN()); // Boolean
+console.log(typeof Number.NaN); // Number
+console.log(typeof String.NaN); // Undefined
