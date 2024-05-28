bb3e3a864168abea5c7f847cd94dd82e52f20f34
Mirza Abdullah Baig <abaig7921@gmail.com>
2024-05-28 14:55:13 +0500

    Object Practice 01

---
+++ Basic_02/objects1.js
@@ -0,0 +1,12 @@
+/*
+Singleton => Constructor sy jo object bny ga wo Singleton object bny ga
+syntax => Object.create => constructor method, esi method ky through singleton bnta hy
+
+Literal sy singleton object nhi bny ga
+*/
+
+// Objects Literals
+
+// Object.create
+
+const user = {}
