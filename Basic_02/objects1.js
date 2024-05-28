28cc50cdeacb17bbe5603863629837f91d7862ba
Mirza Abdullah Baig <abaig7921@gmail.com>
2024-05-28 15:51:42 +0500

    Object Practice 01

--- Basic_02/objects1.js
+++ Basic_02/objects1.js
@@ -5,8 +5,43 @@ syntax => Object.create => constructor method, esi method ky through singleton b
 Literal sy singleton object nhi bny ga
 */
 
+
 // Objects Literals
+const sym = Symbol("sdsd")
 
-// Object.create
+const user = {
+    name: "Abdullah",
+    "friend Name": "Adil",
+    age: 18,
+    [sym]: "Ahad",
+    location: "Karachi",
+    email: "aba@gmail.com",
+    isLoggedIn: false,
+    lastLogin: ["Monday", "Tuesday"]
+}
 
-const user = {}
+// console.log(user.name);
+// // console.log(user.friend Name); // no chance to find the value beacuse of wrong syntax
+// console.log(user["friend Name"]);
+// console.log(user[sym]); // Symbol value
+// console.log(typeof user[sym]); // String
+// console.log(typeof sym); // Symbol
+
+user.email = "abaig@gmail.com";
+// console.log("🚀 ~ email:", user.email)
+
+Object.freeze(user.email);
+
+user.isLoggedIn = true;
+// console.log("🚀 ~ user:", user)
+
+user.greeting = function () {
+    console.log("Hello User!");
+}
+
+user.greeting2 = function () {
+    console.log(`Hello User! ${this.name}`);
+}
+
+console.log(user.greeting());
+console.log(user.greeting2());
