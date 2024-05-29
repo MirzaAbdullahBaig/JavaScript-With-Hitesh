5df9b184b121032f9f3551ab9b0bfb57a81a75a6
Mirza Abdullah Baig <abaig7921@gmail.com>
2024-05-29 17:34:06 +0500

    Function's Practice

--- Basic_03/functions.js
+++ Basic_03/functions.js
@@ -22,6 +22,38 @@ function loginUserMessage (userName = "Sam") {
     return `${userName} Just Logged in`
 }
 
-console.log(loginUserMessage("Abdullah"));
-console.log(loginUserMessage());
+// console.log(loginUserMessage("Abdullah"));
+// console.log(loginUserMessage());
 
+
+
+
+function calculatePrice (...num1) { // rest operator
+    return num1
+}
+
+// console.log(calculatePrice(100, 200, 300));
+
+
+
+const user = {
+    userName: "Abdullah",
+    price: 299
+}
+
+function handleObject(anyobject) {
+    console.log(`Username is ${anyobject.userName} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+// handleObject({userName: "Sufyan", price: 2999})
+
+
+
+const array = [100, 200, 300, 400]
+
+function handleArray(anyarray) {
+    console.log(anyarray[1]);
+}
+// handleArray(array)
+// handleArray([300, 500, 700, 900])
