cbecb8666a22d0942ce02a94763088613bc10631
Mirza Abdullah Baig <abaig7921@gmail.com>
2024-05-30 14:51:36 +0500

    Basic Scope Practice

---
+++ Basic_03/scopes.js
@@ -0,0 +1,27 @@
+if (true) {
+    let a = 10
+    const b = 20
+    var c = 30
+}
+// console.log(c);
+// console.log(b);
+// console.log(a);
+
+
+
+if (true) {
+    var x = 3;
+    // console.log("🚀 ~ x:", x)
+}
+if (true) {
+    let y = 3;
+    // console.log("🚀 ~ y:", y)
+}
+if (true) {
+    const z = 3;
+    // console.log("🚀 ~ z:", z)
+}
+
+// console.log(x)
+// console.log(y)
+// console.log(z)
