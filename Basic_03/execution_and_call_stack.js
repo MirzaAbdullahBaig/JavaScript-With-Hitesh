456d27d6db0473c4bf474a80a1f6dd040c803f2b
Mirza Abdullah Baig <abaig7921@gmail.com>
2024-06-02 22:08:51 +0500

    Execution Context and Call Stack

--- Basic_03/execution_and_call_stack.js
+++ Basic_03/execution_and_call_stack.js
@@ -1,1 +1,12 @@
 // Execution Context and Call Stack in Javascript
+
+let val1 = 10
+let val2 = 5
+
+function addNum(num1, num2) {
+    let total = num1 + num2
+    return total
+}
+
+let result1 = addNum(val1 + val2)
+let result2 = addNum(10 + 2)
