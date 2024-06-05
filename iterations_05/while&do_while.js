4fa92d6a981b0edf9d336ac47d72b84f4d62ef82
Mirza Abdullah Baig <abaig7921@gmail.com>
2024-06-06 00:07:39 +0500

    while & do_while

---
+++ iterations_05/while&do_while.js
@@ -0,0 +1,24 @@
+// While and do while loop
+
+let index = 0
+while (index <= 10) {
+    // console.log(`Value of index is ${index}`);
+    index = index + 2
+}
+
+
+
+let myarray = ["Adil", "Usman", "Jalib", "Ahad", "Okasha"]
+let arr = 0
+
+while (arr < myarray.length) {
+    // console.log(`Value is ${myarray[arr]}`);
+    arr = arr + 1
+}
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <=10)
