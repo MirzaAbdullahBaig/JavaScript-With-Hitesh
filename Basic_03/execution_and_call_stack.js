88c933dfdd88083a8ee28c1b3ec25b89132e6777
Mirza Abdullah Baig <abaig7921@gmail.com>
2024-06-05 11:42:04 +0500

    Executional Context and Call Stack with Excalidraw Diagram

--- Basic_03/execution_and_call_stack.js
+++ Basic_03/execution_and_call_stack.js
@@ -1,4 +1,7 @@
 // Execution Context and Call Stack in Javascript
+// Excalidraw Link => https://excalidraw.com/#json=d8DAUY77BZf-QWip0jo0w,8XSYFuWvCPaAvFras_r5MQ
+
+// Executional Context Example Code
 
 let val1 = 10
 let val2 = 5
@@ -10,3 +13,48 @@ function addNum(num1, num2) {
 
 let result1 = addNum(val1 + val2)
 let result2 = addNum(10 + 2)
+
+
+// Call Stack Example Code
+
+
+// Simple Example
+
+function one() {
+    console.log("one");
+}
+
+function two() {
+    console.log("two");
+}
+
+function three() {
+    console.log("three");
+}
+
+// one();
+// two();
+// three();
+
+
+// Last In First Out (LIFO) Example
+
+function one() {
+    console.log("one");
+    two();
+    console.log("one is end");
+}
+
+function two() {
+    console.log("two");
+    three();
+    console.log("two is end");
+}
+
+function three() {
+    console.log("three");
+}
+
+one();
+two();
+three();
