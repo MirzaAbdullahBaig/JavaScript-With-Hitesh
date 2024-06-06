f6dc0fa7b3f2386ac9cc0a08fce9cc76e7c918df
Mirza Abdullah Baig <abaig7921@gmail.com>
2024-06-06 22:46:33 +0500

    for_in_loop

---
+++ iterations_05/for_in_loop.js
@@ -0,0 +1,34 @@
+// for in loop
+
+
+const myObj = {
+    js: "JavaScript",
+    cpp: "C++",
+    rb: "Ruby",
+    py: "Python",
+    ts: "TypeScript"
+}
+
+for (const key in myObj) {
+    // console.log(`${key} shorcut is ${myObj[key]}`)
+}
+
+
+const progaming = ["JavaScript", "C++", "Ruby", "Python", "Java"]
+
+for (const key in progaming) {
+    // console.log(`Key ${key} is ${progaming[key]}`);
+}
+
+
+
+// const map = new Map();
+// map.set(1, "Abdullah");
+// map.set(2, "Adil");
+// map.set(3, "Ahad");
+// map.set(4, "Ahad");
+
+// for (const key in map) {
+//     console.log(key);
+// }
+// map iteratable nhi hy
