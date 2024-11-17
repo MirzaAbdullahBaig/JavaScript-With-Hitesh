b12aab734f9e42a0ab1baf06b4f24cc109fe9675
Mirza Abdullah Baig <abaig7921@gmail.com>
2024-11-17 11:53:00 +0500

    Beyond_the_Basic_of_Array

---
+++ Beyond_the_Basic_12/Beyond_the_Basic_of_Array.js
@@ -0,0 +1,7 @@
+// Beyond the Basic
+// https://youtu.be/ZRS485LxX0s?si=EWq8_Rv4fFQN8JS4
+
+// There are two types of arrays: holey arrays and continuous arrays. These arrays can contain three types of elements: SMI (Small Integer), Packed Element, and Double.
+
+const myArr = []
+%DebugPrint(myArr)
